7d4bcf4fdbe4410672cfbe46af8d13902384770d
guto <guto@guto-desktop>
2017-03-30 16:10:40 -0300

    iniciando repositorio

--- Exercise2PD/src/exercise2pd/Exercise2PD.java
+++ Exercise2PD/src/exercise2pd/Exercise2PD.java
@@ -15,7 +15,7 @@ public class Exercise2PD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here e
     }
     
 }
